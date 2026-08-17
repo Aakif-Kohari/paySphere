f3091a1306b760fdba5924eddac56fa372045601
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2026-08-17 22:36:43 +0530

    feat(shift): implement shift bidding marketplace with seniority priority & rest period fatigue rules (Closes #1149)

--- backend/src/routes/shiftBidding.routes.js
+++ backend/src/routes/shiftBidding.routes.js
@@ -2,12 +2,19 @@ const express = require('express');
 const auth = require('../middlewares/auth.middleware');
 const { requirePermission } = require('../middlewares/rbac.middleware');
 const { writeRateLimiter } = require('../middlewares/rateLimiter.middleware');
-const { postOpenShift, placeBid, getMarketplace, assignShift } = require('../controllers/shiftBidding.controller');
+const {
+  postOpenShift,
+  placeBid,
+  getMarketplace,
+  checkShiftCompliance,
+  assignShift,
+} = require('../controllers/shiftBidding.controller');
 
 const router = express.Router();
 
 router.post('/marketplace/open', auth, requirePermission('WRITE_EMPLOYEE'), writeRateLimiter, postOpenShift);
 router.get('/marketplace', auth, getMarketplace);
+router.get('/marketplace/compliance-check', auth, checkShiftCompliance);
 router.post('/marketplace/:id/bid', auth, writeRateLimiter, placeBid);
 router.post('/marketplace/:id/assign', auth, requirePermission('WRITE_EMPLOYEE'), writeRateLimiter, assignShift);
 
